580bd0ebbf657a4349bdd685ae1e9ed7aa725f1d
Michael Bozhilov <michaelbozhilov@gmail.com>
2022-10-30 00:47:14 +0300

    Add comments for initial structure

--- index.js
+++ index.js
@@ -4,7 +4,25 @@ const Discord = require('discord.js')
 const client = new Discord.Client({
     intents: [
         Discord.GatewayIntentBits.MessageContent,
+        Discord.GatewayIntentBits.Guilds,
+		Discord.GatewayIntentBits.GuildMessages,
+		Discord.GatewayIntentBits.GuildMembers,
     ]
 });
 
 client.login(process.env.BOT_TOKEN)
+
+// bot structure
+
+// function getRequest(url, expectedResponseCode)
+// function scheduleRequest(url, expectedResponseCode, timeInterval) -> CRON
+
+// function listScheduledRequests -> returns all of the scheduled requests with identification numbers (indexes)
+// function removeScheduledRequest -> delete scheduled request by identification number (index)
+
+client.on("messageCreate", (message) => {
+    console.log(message)
+    if (message.content === "requestah") {
+        message.reply(`Hello Lord ${message.author}`)
+    }
+})

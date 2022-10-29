3679e276ffb770b0afed0bc1e113201c39101795
Michael Bozhilov <michaelbozhilov@gmail.com>
2022-10-30 01:12:53 +0300

    Initial version of request function

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 require('dotenv').config()
+const axios = require('axios')
 
 const Discord = require('discord.js')
 const client = new Discord.Client({
     intents: [
         Discord.GatewayIntentBits.MessageContent,
         Discord.GatewayIntentBits.Guilds,
-		Discord.GatewayIntentBits.GuildMessages,
-		Discord.GatewayIntentBits.GuildMembers,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.GuildMembers,
     ]
 });
 
 client.login(process.env.BOT_TOKEN)
+
+const CHANNEL_ID = "1036019427478622279"
 
 // bot structure
 
@@ -25,4 +28,17 @@ client.on("messageCreate", (message) => {
     if (message.content === "requestah") {
         message.reply(`Hello Lord ${message.author}`)
     }
+    else if (message.content === "check") {
+        request("https://hubconf.thehub-aubg.com", "get", 200)
+    }
 })
+
+async function request(url, requestType, expectedStatusCode) {
+    const res = await axios({
+        method: requestType,
+        url: url,
+    })
+
+    const channel = await client.channels.fetch(CHANNEL_ID)
+    channel.send(`${requestType} request to ${url} exited with code ${res.status}`)
+}
